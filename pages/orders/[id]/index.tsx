63d8a588546c6b81ec4920a58739a7dc6488aab7
Juan Felizzola <juancafelizzola@gmail.com>
2021-09-20 23:43:55 -0500

    Resumen de pago

--- pages/orders/[id]/index.tsx
+++ pages/orders/[id]/index.tsx
@@ -57,7 +57,8 @@ export default function OrderDetails({ initialData }) {
   
   return (
     <>
-      <h1 className="text-xl font-semibold">Orden #{id}</h1>
+      <h1 className="text-xl font-semibold"></h1>
+      <Text fontSize="xl" fontWeight="semibold" mb="2" as="h1">Orden #{id}</Text>
       {order.order_items.map((order_item) => <>
         <Box bg="white" boxShadow="sm" borderRadius="md" px="3" py="2" key={order.id}> 
           <Text lineHeight="initial" mb="1">{order_item.item.title}</Text>
@@ -65,6 +66,24 @@ export default function OrderDetails({ initialData }) {
           <Text color="gray.500" fontSize="sm">{convertToCOP(order_item.full_unit_price)} x {order_item.quantity}</Text>
         </Box>
       </>)}
+
+      <Text my="2" as="h3">Comprador</Text>
+      <Box bg="white" boxShadow="sm" borderRadius="md" px="3" py="2" key={order.id}> 
+        <Text lineHeight="initial" mb="1">{order.buyer.first_name} {order.buyer.last_name}</Text>
+      </Box>
+
+      <Box my="2">
+        <Text as="h3">Resumen del pago</Text>
+        <Text color="gray.500" as="span" fontSize="sm">{order.payments[0].status_detail} | { new Date(order.payments[0].date_approved).toLocaleDateString('es-CO', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</Text>
+      </Box>
+      
+      <Box bg="white" boxShadow="sm" borderRadius="md" px="3" py="2" key={order.id}> 
+        {order.payments.map((payment) => <Box display="flex" justifyContent="space-between" mb="1">
+          <Text fontSize="sm">{payment.reason.substr(0, 30)}...</Text>
+          <Text fontSize="sm">{convertToCOP(payment.total_paid_amount)}</Text>
+        </Box>)}
+      <Text fontWeight="semibold">Total: {convertToCOP(order.total_amount)}</Text>  
+      </Box>
     </>
   )
 }
