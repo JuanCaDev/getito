cfe6d9d5a81c441066e9d1db90bb9d8a9f367da7
Juan Felizzola <juancafelizzola@gmail.com>
2022-04-30 22:40:47 -0500

    dolar api

---
+++ pages/api/dolar.ts
@@ -0,0 +1,36 @@
+import { NextApiHandler } from 'next'
+
+import puppeteer from 'puppeteer';
+
+const handler: NextApiHandler = async (_, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://dolar.wilkinsonpc.com.co/divisas/dolar-diario.html');
+
+    const numbers = await page.evaluate(() => {
+      const numersSpan = document.querySelectorAll('.indicador_numero');
+      const numbers = []
+
+      numersSpan.forEach(span => {
+        const newSpan = span.innerText.replace(/\D+/g, '');
+        numbers.push(Number(newSpan))
+        console.log(numbers)
+      })
+      return numbers
+    });
+
+    const result = {
+      dolarToday: numbers[3],
+      dolarYesterday: numbers[4],
+    }
+
+    await browser.close();
+
+    return res.json({ message: 'ok', data: result });;
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+export default handler
