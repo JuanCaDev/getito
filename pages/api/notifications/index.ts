c11483a372ea25d6e1c9366773bf91e0c808e2a3
Juan Felizzola <juancafelizzola@gmail.com>
2022-05-31 14:08:29 -0500

    no api notification

--- pages/api/notifications/index.ts
+++ pages/api/notifications/index.ts
@@ -6,13 +6,18 @@ import service from 'service'
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const tokenBot = "5435415918:AAGN04IWV6NGM9nLpJGY6YmbWivn3wDLHw8";
-    const response = await axios.post('https://api.telegram.org/bot' + tokenBot + '/sendMessage', {
-      chat_id: '-701539274',
-      text: JSON.stringify(req.body)
-    })
+    // const tokenBot = "5435415918:AAGN04IWV6NGM9nLpJGY6YmbWivn3wDLHw8";
+    // await axios.post('https://api.telegram.org/bot' + tokenBot + '/sendMessage', {
+    //   chat_id: '-701539274',
+    //   text: JSON.stringify(req.body)
+    // })
 
-    console.log(response)
+    // const { data } = await axios.post('https://api.mercadolibre.com' + req.body.resource, {}, {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json',
+    //   }
+    // })  
 
     return res.status(200).json({ message: 'ok' })
   } catch (e) {
